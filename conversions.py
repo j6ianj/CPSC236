77a939aca6a38a656775a7188c9e223ba472f6fc
j6ianj <157513799+j6ianj@users.noreply.github.com>
2024-02-21 14:22:56 -0500

    Add files via upload

---
+++ conversions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def mtoft(m):
+    ft = m / .3048
+    round(ft,2)
+    return ft
+def fttom(ft):
+    m = ft * .3048
+    round(m,2)
+    return m
+
---
+++ salestax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+stax = .06
+tax = 0
+def SalesTax(cost):
+    tax= cost * stax
+    return round(tax,2)
+
+    
+
+
+# In[ ]:
+
+
+
+
